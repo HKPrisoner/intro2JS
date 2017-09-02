bab9595643fae2e52941bec8ae888a9ea7302772
HKPrisoner <fu1225@sohu.com>
2017-09-02 14:25:56 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//console.log("Hello world")
+
+var http=require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world!');
+}).listen(8080);
