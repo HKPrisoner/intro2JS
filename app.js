b5fde747145c375fa86e33c088a953476819c5f1
HKPrisoner <fu1225@sohu.com>
2017-09-02 14:45:25 +0800

    ......

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var dt = require('./myfirstmodule')
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("The current time is :" + dt.myDateTime());
-    res.end('Hello world!');
+//    res.write("The current time is :" + dt.myDateTime());
+    res.write(req.url);
+    res.end();
+//    res.end('Hello world!');
 }).listen(8080);
 
 console.log("Listening on 8080")
