c54e30eece028652bfd00d3645b7ed660b621458
HKPrisoner <fu1225@sohu.com>
2017-09-02 14:52:58 +0800

    add query

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 var http=require('http');
 var dt = require('./myfirstmodule')
+var url = require('url')
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
 //    res.write("The current time is :" + dt.myDateTime());
-    res.write(req.url);
-    res.end();
+//    res.write(req.url);
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
 //    res.end('Hello world!');
 }).listen(8080);
 
